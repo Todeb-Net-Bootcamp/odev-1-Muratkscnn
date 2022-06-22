7220a97cb44ddf33e591780f2ead10597b270adf
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-06-22 18:06:03 +0300

    22.06.22

---
+++ Personelinformation.Data/Config/DetailInformationConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Personelinformation.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personelinformation.Data.Config
+{
+    public class DetailInformationConfig : IEntityTypeConfiguration<DetailInformation>
+    {
+        public void Configure(EntityTypeBuilder<DetailInformation> builder)
+        {
+            builder.HasData(
+
+                new DetailInformation() { DetailInformationID = 1, FatherName = "Mehmet", MotherName = "Ayşe", BirthPlace = "Kilis" },
+                new DetailInformation() { DetailInformationID = 2, FatherName = "Ahmet", MotherName = "Aslı", BirthPlace = "İstanbul" },
+                new DetailInformation() { DetailInformationID = 3, FatherName = "Hasan", MotherName = "Müge", BirthPlace = "Rize" }
+
+
+                );
+        }
+    }
+}
---
+++ Personelinformation.Data/Config/InformationConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Personelinformation.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personelinformation.Data.Config
+{
+    class InformationConfig : IEntityTypeConfiguration<Information>
+    {
+        public void Configure(EntityTypeBuilder<Information> builder)
+        {
+            builder.HasData(
+            new Information { InformationID = 1, PersonelName = "Murat", PersonelSurname = "Kuşcan", DetailInformationID = 1, PersonelInformationID = 1 },
+               new Information { InformationID = 2, PersonelName = "Adnan", PersonelSurname = "Derkan", DetailInformationID = 2, PersonelInformationID = 2 },
+               new Information { InformationID = 3, PersonelName = "Esra", PersonelSurname = "Kurt", DetailInformationID = 3, PersonelInformationID = 3 }
+
+               );
+        }
+    }
+}
